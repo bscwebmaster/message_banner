a5d2d1244f39bdcd058059c3046b08d23eb16d46
Sophie Shanahan-Kluth <sophie.sk@focusrite.com>
2019-09-19 09:10:20 +0100

    Start on settings form.

---
+++ src/Form/MessageBannerSettingsForm.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\message_banner\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * The message banner settings form.
+ */
+class MessageBannerSettingsForm extends ConfigFormBase {
+
+
+
+}
